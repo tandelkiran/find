937af31297c04ed15f603c65bc24f76246062392
tandelkiran <tandelkiran7698@gmail.com>
2020-02-24 10:29:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jpdxm8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jpdxm8)
---
+++ index.ts
@@ -0,0 +1,22 @@
+import './style.css';
+
+import { fromEvent } from 'rxjs'; 
+import { find, repeatWhen, mapTo, startWith, filter} from 'rxjs/operators';
+
+// elem ref
+const status = document.getElementById('status');
+
+// streams
+const clicks$ = fromEvent(document, 'click');
+
+clicks$
+  .pipe(
+    find((event: any) => event.target.id === 'box'),
+    mapTo('Found!'),
+    startWith('Find me!'),
+    // reset when click outside box
+    repeatWhen(() =>
+      clicks$.pipe(filter((event: any) => event.target.id !== 'box'))
+    )
+  )
+  .subscribe(message => (status.innerHTML = message));
